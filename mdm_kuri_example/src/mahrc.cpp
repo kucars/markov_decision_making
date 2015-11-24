bde1b0d52a4a79ff3d9b579624086bc4a972cbff
Tarek Taha <tarek.taha@gmail.com>
2015-11-24 11:14:16 +0400

    -fix

--- mdm_kuri_example/src/mahrc.cpp
+++ mdm_kuri_example/src/mahrc.cpp
@@ -145,7 +145,7 @@ int main ( int argc, char** argv )
         // i removed the vic_dan so the index will change now
         // S -> O -> A -> S -> O -> A ...
 
-        int observations[5]= {-1,2,5,6,8};
+        int observations[5]= {-1,5,5,6,8};
         double eta = 0;
 
         for(int i=0;i<sizeof(observations)/sizeof(int);i++)
