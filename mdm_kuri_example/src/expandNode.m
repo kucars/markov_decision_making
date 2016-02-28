dfb63527f9bd8d6eb98e168fbda1e87f1b8d5f14
Tarek Taha <tarek.taha@gmail.com>
2016-02-28 13:38:19 +0400

    implementing time function

---
+++ mdm_kuri_example/src/expandNode.m
@@ -0,0 +1,3 @@
+function [] = expandNode() 
+
+end
---
+++ mdm_kuri_example/src/rewardsController_v1.m
@@ -0,0 +1,9 @@
+%Description: this function take the criteria : time, energy, distance to danger ----
+% for the time, if there are multiple paths, check the node after the common ones, if it took the node that leads to the longer path then there will be penalization for that. 
+%This code might have some bugs 
+%Hend AlTair - hend.altair@kustar.ac.ae 
+
+function[]=rewardsController_v1[]
+
+
+end 
